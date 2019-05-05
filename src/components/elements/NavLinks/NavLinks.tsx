1c4c566fe7f821da5b81c4e12a4d26005ead2098
vince abruzzo <vgabruzzo@gmail.com>
2019-05-05 10:46:05 -0400

    veggie menu

--- src/components/elements/NavLinks/NavLinks.tsx
+++ src/components/elements/NavLinks/NavLinks.tsx
@@ -1,22 +1,38 @@
-import React, { StatelessComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { Link } from '@reach/router';
 import styles from './NavLinks.module.scss';
 
-const NavLinks: StatelessComponent = () => (
-  <ul className={styles.linkList}>
-    <li className={styles.linkLi}>
-      <Link to="/">Home</Link>
-    </li>
-    <li className={styles.linkLi}>
-      <Link to="/classify">Classify</Link>
-    </li>
-    <li className={styles.linkLi}>
-      <Link to="/vent">Vent</Link>
-    </li>
-    <li className={styles.linkLi}>
-      <Link to="/about">About</Link>
-    </li>
-  </ul>
-);
+const NavLinks: FunctionComponent = () => {
+  const [veggieOpen, setVeggieOpen] = useState(false);
+
+  const toggleVeggieOpen = () => setVeggieOpen(!veggieOpen);
+
+  return (
+    <div>
+      <span className={styles.menu} onClick={toggleVeggieOpen}>
+        Menu
+      </span>
+      <ul
+        className={`${styles.linkList} ${
+          veggieOpen ? styles.mobileActive : ''
+        }`}
+        onClick={toggleVeggieOpen}
+      >
+        <li className={styles.linkLi}>
+          <Link to="/">Home</Link>
+        </li>
+        <li className={styles.linkLi}>
+          <Link to="/classify">Classify</Link>
+        </li>
+        <li className={styles.linkLi}>
+          <Link to="/vent">Vent</Link>
+        </li>
+        <li className={styles.linkLi}>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default NavLinks;
